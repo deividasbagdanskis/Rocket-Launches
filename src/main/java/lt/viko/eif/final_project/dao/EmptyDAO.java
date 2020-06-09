855209309dcab7074f5b15dfcdf2be7534469091
Deividas <51085446+deividasbagdanskis@users.noreply.github.com>
2020-06-09 11:45:53 +0300

    added empty classes

---
+++ src/main/java/lt/viko/eif/final_project/dao/EmptyDAO.java
@@ -0,0 +1,7 @@
+package lt.viko.eif.final_project.dao;
+
+/**
+ * @author Deividas Bagdanskis
+ */
+public class EmptyDAO {
+}
---
+++ src/main/java/lt/viko/eif/final_project/pojos/EmptyClass.java
@@ -0,0 +1,7 @@
+package lt.viko.eif.final_project.pojos;
+
+/**
+ * @author Deividas Bagdanskis
+ */
+public class EmptyClass {
+}
